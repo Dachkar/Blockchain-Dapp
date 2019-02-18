0899cb71dfa46ccfe5d1c0e438d89fef4993b630
Dachkar <34558512+Dachkar@users.noreply.github.com>
2019-02-18 09:04:33 -0500

    Added default money ammt to each new registered user (under profile)

--- blockchainDapp/users/models.py
+++ blockchainDapp/users/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    money = models.DecimalField(max_digits=8, decimal_places=2, default=500)
 
     def __str__(self):
         return f'{self.user.username} Profile'
